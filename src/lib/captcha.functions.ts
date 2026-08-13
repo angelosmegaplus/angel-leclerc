a44e0a07c28f68447d3727bb3bcf106aed0dba6f
angelosmegaplus <angelleclerc2006@gmail.com>
2026-08-13 10:50:57 +0200

    chore(captcha): remove obsolete server challenge generator

--- src/lib/captcha.functions.ts
+++ src/lib/captcha.functions.ts
@@ -1,10 +1,5 @@
 import { createServerFn } from "@tanstack/react-start";
 
-export const getCaptchaChallenge = createServerFn({ method: "GET" }).handler(async () => {
-  const { createChallenge } = await import("./captcha.server");
-  return createChallenge();
-});
-
 /** Vérification serveur du calcul anti-robot local. */
 export const verifyCaptchaAnswer = createServerFn({ method: "POST" })
   .inputValidator((input: { token: string; answer: string }) => ({
