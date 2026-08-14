98e9d3b589b4963f6b5ece8f243acf449afd78e6
angelosmegaplus <angelleclerc2006@gmail.com>
2026-08-14 15:00:49 +0200

    Réintègre la maintenance dans le shell du site

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,6 +23,7 @@ import { CartDrawer } from "../components/CartDrawer";
 import { PwaRegistrar } from "../components/PwaRegistrar";
 import { ThemeSync } from "../components/ThemeController";
 import { AngelOSCardStyle } from "../components/AngelOSCardStyle";
+import { MaintenanceGate } from "../components/MaintenanceGate";
 import { THEME_INIT_SCRIPT } from "../lib/theme";
 
 function NotFoundComponent() {
@@ -130,33 +131,35 @@ function RootComponent() {
 
   useEffect(() => {
     void bootAngelOS().catch((error) => {
-      console.warn("Angel OS passive runtime unavailable", error);
+      console.warn("Angel OS runtime unavailable", error);
     });
   }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeSync />
-      <AngelOSCardStyle />
-      <PageViewTracker />
-      <PwaRegistrar />
-      {isAngelOSPage || isAdminPage ? (
-        <main className="min-h-screen">
-          <Outlet />
-        </main>
-      ) : (
-        <div className="flex min-h-screen flex-col">
-          <ApprenticeshipBanner />
-          <Header />
-          <main className="flex-1">
+      <MaintenanceGate bypass={isAdminPage}>
+        <ThemeSync />
+        <AngelOSCardStyle />
+        <PageViewTracker />
+        <PwaRegistrar />
+        {isAngelOSPage || isAdminPage ? (
+          <main className="min-h-screen">
             <Outlet />
           </main>
-          <Footer />
-        </div>
-      )}
-      {!isAngelOSPage && !isAdminPage && <CartDrawer />}
-      <Toaster position="top-center" />
-      <Analytics />
+        ) : (
+          <div className="flex min-h-screen flex-col">
+            <ApprenticeshipBanner />
+            <Header />
+            <main className="flex-1">
+              <Outlet />
+            </main>
+            <Footer />
+          </div>
+        )}
+        {!isAngelOSPage && !isAdminPage && <CartDrawer />}
+        <Toaster position="top-center" />
+        <Analytics />
+      </MaintenanceGate>
     </QueryClientProvider>
   );
 }
