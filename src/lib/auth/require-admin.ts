85d38ab8d08b0c583face16ad4e07c17d39aa2d0
angelosmegaplus <angelleclerc2006@gmail.com>
2026-08-14 14:40:24 +0200

    Corrige le helper d'autorisation admin

--- src/lib/auth/require-admin.ts
+++ src/lib/auth/require-admin.ts
@@ -1,10 +1,4 @@
-import { requireAngelAuth } from './require-angel-auth';
-import { createMiddleware } from '@tanstack/react-start';
 import { resolveUserRole } from './roles';
-
-export const requireAdmin = createMiddleware({ type: 'function' }).server(async ({ next, context }) => {
-  return requireAngelAuth._types.server.middleware(async () => next()) as never;
-});
 
 export async function assertAngelAdmin(context: any) {
   const role = await resolveUserRole(context);
