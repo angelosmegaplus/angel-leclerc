2c1fac077f33ba9065d0267381af93242d6a83fb
Angel OS <angelleclerc2006@gmail.com>
2026-08-15 14:22:38 +0200

    Force publication of political salaries article

--- src/lib/legacy-git-articles.ts
+++ src/lib/legacy-git-articles.ts
@@ -3,6 +3,7 @@ import { macronPhilippeArticle } from "@/content/macronPhilippeArticle";
 import { politicalSalariesArticle } from "@/content/politicalSalariesArticle";
 import type { Article } from "@/lib/articles-types";
 
+// Les articles maintenus dans Git restent visibles même si la base est temporairement indisponible.
 export const legacyGitArticles = [
   politicalSalariesArticle as unknown as Article,
   macronPhilippeArticle as unknown as Article,
