385a281abf0cee3003ab189f8cdcdbd9188b3095
Angel OS <angelleclerc2006@gmail.com>
2026-08-23 14:14:54 +0200

    Branche Mistral sur la diversification du fil

--- src/lib/flamme-news.functions.ts
+++ src/lib/flamme-news.functions.ts
@@ -1,5 +1,6 @@
 import { createServerFn } from "@tanstack/react-start";
 import { loadFlammeNews } from "./flamme-news.server";
+import { curateFlammeNewsWithMistral } from "./flamme-news-curation.server";
 
 export const getFlammeNews = createServerFn({ method: "GET" })
   .inputValidator((data: unknown) => {
@@ -7,5 +8,6 @@ export const getFlammeNews = createServerFn({ method: "GET" })
     return { region: typeof region === "string" ? region : null };
   })
   .handler(async ({ data }) => {
-    return loadFlammeNews(data.region);
+    const payload = await loadFlammeNews(data.region);
+    return curateFlammeNewsWithMistral(payload);
   });
